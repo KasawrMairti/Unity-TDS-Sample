7761db824db0434691427193db41b3520731fcf9
KasawrMairti(카쇼르) <bsy50642@naver.com>
2025-02-27 16:39:49 +0900

    배경 자동 이동 임시 저장
    배경 이동 구현

--- Assets/5.Scripts/System/BackgroundImage.cs
+++ Assets/5.Scripts/System/BackgroundImage.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BackgroundImage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Debug.Log(spriteRenderer.size.x);
+
+        // 이미지 일정 거리이상 지나가면 순환시켜 무한정 이동하는 느낌을 구현
+        if (transform.position.x < spriteRenderer.size.x)
+        {
+            transform.position = new Vector3(spriteRenderer.size.x, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * 1.0f * Time.deltaTime);
+        }
     }
 }
